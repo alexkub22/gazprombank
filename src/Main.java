782ead6b16aaea7ac159678abc7036d7e37f53df
alexkub22 <alexkub22@yandex.ru>
2019-11-24 23:18:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public int sum(int[ ] arr){
+            int sum=0;
+            for(int i: arr) {
+                sum +=i;
+            }
+            return sum;
+    }
